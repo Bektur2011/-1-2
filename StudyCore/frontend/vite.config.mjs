4f9053ebb2d24f726ab1ca0c74e59819c1cbc83b
Bektur2011 <b3474850@gmail.com>
2026-01-15 11:13:27 +0600

    Set build output directory in vite.config.mjs
    Configure build output directory for frontend.

--- StudyCore/frontend/vite.config.mjs
+++ StudyCore/frontend/vite.config.mjs
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../backend/frontend/dist", // билд попадёт в backend
+    emptyOutDir: true
+  }
 });
