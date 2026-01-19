d4529e48fc38eafdf8f4e49cd97c30b8ae2aafaf
Bektur2011 <b3474850@gmail.com>
2026-01-19 23:28:13 +0600

    Fix API calls, add mobile responsiveness for iOS/Android, improve styles and spacing

--- StudyCore/frontend/src/api/users.api.js
+++ StudyCore/frontend/src/api/users.api.js
@@ -3,8 +3,8 @@ import API from "./axios";
 // Получить всех пользователей
 export const getUsers = async () => {
   try {
-    const response = await API.get("/users");
-    return response.data;
+    const response = await API.get("/api/users");
+    return response;
   } catch (err) {
     throw err.response ? err.response.data : { error: "Ошибка сервера" };
   }
